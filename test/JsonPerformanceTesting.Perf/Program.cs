9a338f24744f02889a93506503ea0d8a120c62dd
Michael Robertson <michael.robertson@tranzact.net>
2021-05-05 09:36:29 -0400

    Add test and perf projects

---
+++ test/JsonPerformanceTesting.Perf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonPerformanceTesting.Perf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
